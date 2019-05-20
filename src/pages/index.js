8eb9bf50008b501eefecb16434f3538126b44911
Ellie Grace <info@builtbyellie.com>
2019-05-20 17:38:52 +0200

    add illustration

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,28 +11,41 @@ const IndexPage = (props) => (
           title="Home Page"
           keywords={[`blog`, `gatsby`, `javascript`, `react`]}
         />
-            
+        <Img fluid={props.data.imageFour.childImageSharp.fluid} style={{margin: `10vh auto 2vh auto`, width: `400px`, alignSelf: `center`}} />
 
-  <div className="banner" style={{minHeight: `80vh`, display: `flex`, flexDirection: `column`, justifyContent: `center`, alignItems: `center` }}>
-   
+        <div className="banner" style={{ height: `20vh`, width: `100%`, display: `flex`, flexDirection: `column`, justifyContent: `flex-start`, alignItems: `center` }}>
 
-    <h2 style={{fontSize: `36px`, color: `#545459`, marginTop: `-10px`}}>My name is Ellie, and I build websites*</h2>
-    <h6 style={{fontSize: `12px`, color: `#545459`, marginTop: `-20px`}}>*and do some other stuff as well.</h6>
-    
+    <h2 style={{zIndex: `100`, opacity: `100%`, fontSize: `36px`, color: `#545459`, marginTop: `-10px`}}>My name is Ellie, and I build websites*</h2>
+    <h6 style={{fontSize: `12px`, color: `#545459`, marginTop: `-20px`}}>*and do some other cool things as well.</h6>
 
-  </div>
-  {/* <Img fluid={props.data.imageOne.childImageSharp.fluid} /> */}
-<Img fluid={props.data.imageTwo.childImageSharp.fluid} />
-<Img fluid={props.data.imageThree.childImageSharp.fluid} />
+            </div>
 
-<BackgroundImage 
+
+        {/* <BackgroundImage 
                             Tag="section"
                            fluid={props.data.imageOne.childImageSharp.fluid}
                            backgroundColor={`#040e18`}
-                           style={{height: `100vh`, display: `flex`, justifyContent: `center`, alignItems: `center`}}
+
+                           style={{
+                            
+                            height: `100vh`, display: `flex`, justifyContent: `center`, alignItems: `center`}}
           >
-            <h1 style={{color: `white`}}>Hello gatsby-background-image</h1>
+              
+           
+  
           </BackgroundImage>
+            
+
+    */}
+
+    
+
+  
+  {/* <Img fluid={props.data.imageOne.childImageSharp.fluid} /> */}
+{/* <Img fluid={props.data.imageTwo.childImageSharp.fluid} />
+<Img fluid={props.data.imageThree.childImageSharp.fluid} /> */}
+
+
        )
 
   </Layout>
@@ -42,7 +55,7 @@ export default IndexPage
 
 export const pageQuery = graphql`
 query {
-  imageOne: file(relativePath: { eq: "one.jpg" }) {
+  imageOne: file(relativePath: { eq: "test.JPG" }) {
     childImageSharp {
       fluid(quality: 90, maxWidth: 4160) {
         ...GatsbyImageSharpFluid_withWebp
@@ -64,6 +77,14 @@ query {
       }
     }
   }
+  imageFour: file(relativePath: { eq: "icon.png" }) {
+    childImageSharp {
+      fluid(maxWidth: 500) {
+        ...GatsbyImageSharpFluid
+      }
+    }
+  }
+ 
 }
 `
 
